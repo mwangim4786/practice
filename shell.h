7b4e4452dd72493b76e212bd3674b2caaa2ff530
MKihungi <mwangim4786@example.com>
2023-05-24 13:19:32 -0700

    dfghjk

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void prompt(char **av, char **env);
+
+#endif
